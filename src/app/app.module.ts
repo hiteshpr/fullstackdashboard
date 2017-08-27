3f0a3527cceef9a97673f8e7409b5debae527383
Hitesh Roy <hiteshproy@gmail.com>
2017-08-27 12:45:12 +0530

    added the home page. Images and routing all working fine.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      { path:'', component:HomeComponent},
+      ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
